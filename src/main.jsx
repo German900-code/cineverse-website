f631d37bbedd4c39590aedeec63c9b4efad63d99
German Voloshyn <germanredmi13@gmail.com>
2026-08-29 11:54:49 +0300

    Fix router basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -14,51 +14,56 @@ import Favorites from "./components/Favorites.jsx";
 import TrendingMovies from "./components/TrendingMovies.jsx";
 import SearchResults from "./components/SearchResults.jsx";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <NotFoundPage />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "movie/:id",
+          element: <Details />,
+        },
+        {
+          path: "tv-show/:id",
+          element: <Details />,
+        },
+        {
+          path: "movies",
+          element: <Movies />,
+        },
+        {
+          path: "tv-shows",
+          element: <TVShows />,
+        },
+        {
+          path: "favorites",
+          element: <Favorites />,
+        },
+        {
+          path: "trending",
+          element: <TrendingMovies />,
+        },
+        {
+          path: "search",
+          element: <SearchResults />,
+        },
+        {
+          path: "*",
+          element: <NotFoundPage />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    errorElement: <NotFoundPage />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "movie/:id",
-        element: <Details />,
-      },
-      {
-        path: "tv-show/:id",
-        element: <Details />,
-      },
-      {
-        path: "movies",
-        element: <Movies />,
-      },
-      {
-        path: "tv-shows",
-        element: <TVShows />,
-      },
-      {
-        path: "favorites",
-        element: <Favorites />,
-      },
-      {
-        path: "trending",
-        element: <TrendingMovies />,
-      },
-      {
-        path: "search",
-        element: <SearchResults />,
-      },
-      {
-        path: "*",
-        element: <NotFoundPage />,
-      },
-    ],
+    basename: "/cinewave-website",
   },
-]);
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
